514e721c76334f48df7b8e9f931227f6ac1c0597
Siarhei Rymasheuski <siarheiry@playtika.com>
2022-02-03 19:43:15 +0300

    Add FX

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,14 +6,16 @@ public class Bomb : MonoBehaviour
 {
     [SerializeField] private GameObject explosionPrefab;
     [SerializeField] private LayerMask explosionMask;
+    private int j;
     void Start()
     {
         Invoke("Explode", 3f);
+        var _vector = new Vector2(1, 1);
     }
     
     void Explode()
     {
-        //Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
         var hit = Physics2D.OverlapCircleAll(transform.position, 1f,explosionMask);
         foreach (var collider in hit)
         {
